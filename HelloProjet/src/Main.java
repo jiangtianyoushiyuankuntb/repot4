7758f2b21525a722296ebd0f12e57accdb552548
jiang <jiang@itcast.cn>
2018-11-04 21:16:11 +0800

    修改后的代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,13 +1,13 @@
-import org.junit.Test;
+
 
 public class Main {
 
-    @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
+
+    public static void main(String[] args) {
+        System.out.println("git,hello");
+        System.out.println("git,hello");
+        System.out.println("git,hello");
+        System.out.println("git,hello");
+        System.out.println("git,hello");
     }
 }
